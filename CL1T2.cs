2d74e5487f80c622ca6da4bd8e4f283a4e1355e0
LordFrodo <47020667+LordFrodo@users.noreply.github.com>
2019-01-29 22:46:17 -0500

    Add files via upload
    Add Tarea :V

---
+++ CL1T2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de datos
+            Console.WriteLine("Ingrese t, a");
+            double t = double.Parse(Console.ReadLine());
+            double aGrados = double.Parse(Console.ReadLine());
+
+            //Valores cte
+            double bGrados = 90;
+
+            //Transformacion a radianes
+            double a = aGrados * (Math.PI / 180);
+            double b = bGrados * (Math.PI / 180);
+
+            //Calculo por ley de senos y pitagoras
+            double y = (t * Math.Sin(a)) / Math.Sin(b);
+            double z = Math.Sqrt((t * t) - (y * y));
+            double cRad = Math.Asin((z * Math.Sin(b)) / t);
+            double c = cRad * (180 / Math.PI);
+
+
+            //Resultados
+            Console.WriteLine("y:" + y);
+            Console.WriteLine("z:" + z);
+            Console.WriteLine("c:" + c);
+            Console.ReadLine();
+
+        }
+    }
+}
