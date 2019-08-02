ea18441f0e3966692e5b527f93ab9b95cacba431
LordFrodo <47020667+LordFrodo@users.noreply.github.com>
2019-08-02 07:43:09 -0500

    Add files via upload
    Creacion y uso de clases

---
+++ Hamburguesa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Hamburguesa
+    {
+        private string nombre;
+        private bool vegetariana;
+
+        public bool EsComidaDeVegano()
+        {
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hamburguesa hamburguesa;//Declaracion del objeto
+            hamburguesa = new Hamburguesa();
+            Console.Write(hamburguesa.EsComidaDeVegano() ? "Es comida de verano" : "No vives de ensalada"); //Represantacion de un if de forma abreviada
+            Console.ReadKey(); //No se cierre la ventana
+        }
+    }
+}
